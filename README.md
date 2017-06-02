e28e2c1fdc22611fd346ca336cc3bc10e66ccc31
zhoujiping <zhoujiping@zhoujiping.com>
2017-06-03 00:58:24 +0800

    修改Readme.md

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# my-php-framework
+# 论PHP框架是如何诞生的?
+
+这是第一次重构后的代码, 文章地址： http://www.zhoujiping.com/php/build-your-own-php-framework/#comments
+
--- functions.php
+++ functions.php
@@ -3,7 +3,7 @@
  function connectToDb()
  {
     try {
-        return new PDO('mysql:host=127.0.0.1;dbname=todolist', 'root', 'longzu2016!@#');
+        return new PDO('mysql:host=127.0.0.1;dbname=todolist', 'root', '123456');
     } catch (PDOException $e) {
         die($e->getMessage());
     }
