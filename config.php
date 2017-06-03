2f8b7dd4a05c03dabea79bf694550b81eb403a31
zhoujiping <zhoujiping@zhoujiping.com>
2017-06-03 23:12:50 +0800

    代码修正

--- config.php
+++ config.php
@@ -6,6 +6,8 @@ return [
         'username'      => 'root',
         'password'     => '123456',
         'connection'    => 'mysql:host=127.0.0.1',
-        'options'       => []
+        'options'       => [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+        ]
     ]
  ];
